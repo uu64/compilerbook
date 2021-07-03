d62de035f1c8254a3b965244f0491581ba02f280
uu64 <yusuke.t.0808@gmail.com>
2021-07-04 01:44:35 +0900

    update: tokenizer

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -6,6 +6,33 @@
 #include <string.h>
 
 
+// 入力プログラム
+char *user_input;
+
+// エラーを報告するための関数
+// printfと同じ引数を取る
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n", user_input);
+    fprintf(stderr, "%*s", pos, " ");
+    fprintf(stderr, "^ ");
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
+
 // トークンの種類
 typedef enum {
     TK_RESERVED, // 記号
@@ -25,6 +52,76 @@ struct Token {
 
 // 現在着目しているトークン
 Token *token;
+
+// 新しいトークンを作成してcurに繋げる
+Token *new_token(TokenKind kind, Token *cur, char *str) {
+    Token *tok = calloc(1, sizeof(Token));
+    tok->kind = kind;
+    tok->str = str;
+    cur->next = tok;
+    return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize(char *p) {
+    Token head;
+    head.next = NULL;
+    Token *cur = &head;
+
+    while (*p) {
+        /* 空白文字をスキップ */
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
+
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
+            cur = new_token(TK_RESERVED, cur, p++);
+            continue;
+        }
+
+        if (isdigit(*p)) {
+            cur = new_token(TK_NUM, cur, p);
+            cur->val = strtol(p, &p, 10);
+            continue;
+        }
+        error_at(p, "トークナイズできません");
+    }
+
+    new_token(TK_EOF, cur, p);
+    return head.next;
+}
+
+/* 次のトークンが期待している記号のときにはトークンを1つ読み進めて真を返す。 */
+/* それ以外の場合には偽を返す。 */
+bool consume(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        return false;
+    token = token->next;
+    return true;
+}
+
+/* 次のトークンが期待している記号のときにはトークンを1つ読み進める。 */
+/* それ以外の場合にはエラーを報告する。 */
+void expect(char op) {
+    if (token->kind != TK_RESERVED || token->str[0] != op)
+        error_at(token->str, "'%c'ではありません");
+    token = token->next;
+}
+
+/* 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。 */
+/* それ以外の場合にはエラーを報告する。 */
+int expect_number() {
+    if (token->kind != TK_NUM )
+        error_at(token->str, "数ではありません");
+    int val = token->val;
+    token = token->next;
+    return val;
+}
+
+bool at_eof() {
+    return token->kind == TK_EOF;
+}
 
 
 // 抽象構文木のノードの種類
@@ -60,6 +157,10 @@ Node *new_node_num(int val) {
     node->val = val;
     return node;
 }
+
+Node *expr();
+Node *mul();
+Node *primary();
 
 Node *expr() {
     Node *node = mul();
@@ -100,102 +201,38 @@ Node *primary() {
 }
 
 
-/* エラーを報告するための関数 */
-/* printfと同じ引数を取る */
-void error(char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
-}
-
-/* 入力プログラム */
-char *user_input;
-
-/* エラー箇所を報告する */
-void error_at(char *loc, char *fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-
-    int pos = loc - user_input;
-    fprintf(stderr, "%s\n", user_input);
-    fprintf(stderr, "%*s", pos, " ");
-    fprintf(stderr, "^ ");
-    vfprintf(stderr, fmt, ap);
-    fprintf(stderr, "\n");
-    exit(1);
-}
-
-/* 次のトークンが期待している記号のときにはトークンを1つ読み進めて真を返す。 */
-/* それ以外の場合には偽を返す。 */
-bool consume(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op)
-        return false;
-    token = token->next;
-    return true;
-}
-
-/* 次のトークンが期待している記号のときにはトークンを1つ読み進める。 */
-/* それ以外の場合にはエラーを報告する。 */
-void expect(char op) {
-    if (token->kind != TK_RESERVED || token->str[0] != op)
-        error_at(token->str, "'%c'ではありません");
-    token = token->next;
-}
-
-/* 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。 */
-/* それ以外の場合にはエラーを報告する。 */
-int expect_number() {
-    if (token->kind != TK_NUM )
-        error_at(token->str, "数ではありません");
-    int val = token->val;
-    token = token->next;
-    return val;
-}
-
-bool at_eof() {
-    return token->kind == TK_EOF;
-}
-
-/* 新しいトークンを作成してcurに繋げる */
-Token *new_token(TokenKind kind, Token *cur, char *str) {
-    Token *tok = calloc(1, sizeof(Token));
-    tok->kind = kind;
-    tok->str = str;
-    cur->next = tok;
-    return tok;
-}
-
-/* 入力文字列pをトークナイズしてそれを返す */
-Token *tokenize(char *p) {
-    Token head;
-    head.next = NULL;
-    Token *cur = &head;
-
-    while (*p) {
-        /* 空白文字をスキップ */
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        if (*p == '+' || *p == '-') {
-            cur = new_token(TK_RESERVED, cur, p++);
-            continue;
-        }
-
-        if (isdigit(*p)) {
-            cur = new_token(TK_NUM, cur, p);
-            cur->val = strtol(p, &p, 10);
-            continue;
-        }
-        error_at(p, "トークナイズできません");
+// スタックマシンの実装
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
     }
 
-    new_token(TK_EOF, cur, p);
-    return head.next;
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
 }
+
 
 int main(int argc, char **argv) {
     if (argc != 2) {
